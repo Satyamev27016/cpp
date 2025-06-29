7e41b76357321c08c24dbde1a9587476dac1931f
satyamev <satyamev701600@gmail.com>
2025-06-29 11:25:14 +0530

    Add implementation of lower bound algorithm with corresponding executable

---
+++ binary_search/lowerbound.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerbound(int arr[],int n, int target){
+ int low =0;
+ int high = n-1;
+ int ans;
+
+ while(high >=low){
+    int mid = (low+high)/2;
+    if(mid >= target){
+        ans = mid;
+        high = mid-1;
+    }else{
+        low = mid+1;
+    }
+ }
+ return ans;
+
+}
+
+int main(){
+    int arr1[] = {1,2,3,4,5,6};
+    int n = 6;
+    int target=2;
+    int ans = lowerbound(arr1,n,target);
+    cout<<ans<<endl;
+    return 0;
+}
